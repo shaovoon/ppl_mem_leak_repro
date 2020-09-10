a7a82ceaf8836cbb1e6ed1bb5f35caad6571e229
shaovoon <shaovoon@yahoo.com>
2020-09-10 13:42:09 +0800

    Add C++17 parallel for_each and PPL parallel for loop examples

--- TestPPL/TestPPL.cpp
+++ TestPPL/TestPPL.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <ppl.h>
+#include <algorithm>
+#include <execution>
 
 int main()
 {
 	std::vector<int> vec(1000);
 	memset(vec.data(), 0, vec.size() * sizeof(int));
 
+	/*
+	// PPL parallel for_each has no memory leak
 	using namespace concurrency;
 	parallel_for_each(vec.begin(), vec.end(), [&](int& num) {
 		++num;
 		});
-
-    std::cout << "Done!\n";
+	*/
+	/*
+	// C++ 17 parallel for_each has no memory leak
+	std::for_each(std::execution::par, vec.begin(), vec.end(), [&](int& num) {
+		++num;
+		});
+	*/
+	using namespace concurrency;
+	parallel_for(0U, vec.size(), [&](unsigned int index) {
+		++vec[index];
+		});
+	std::cout << "Done!\n";
 }
